ef026b88ea8b5f4dff391145e8b500f5d53e54ca
j-ospina <47672032+j-ospina@users.noreply.github.com>
2019-03-13 21:35:03 -0400

    Driver files

---
+++ Code/xbee3.c
@@ -0,0 +1,21 @@
+#include <msp430.h>
+#include <eusci_a_uart.h>
+
+void xbee3_initialize(
+
+        //set Network ID
+        /*
+         *
+         */
+
+        //set Channel
+
+        //read Serial High
+        //read Serial Low
+
+        //write Node Identifier **Optional**
+
+        //Device Role
+
+
+        )
---
+++ Code/xbee3.h
@@ -0,0 +1,11 @@
+#include <msp430.h>
+#include <eusci_a_uart.h>
+
+#ifndef XBEE3_H_
+#define XBEE3_H_
+
+
+
+
+
+#endif /* XBEE3_H_ */
