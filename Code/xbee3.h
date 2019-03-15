8a4d20e6441a51dd63cb2448a2bd5e9edbc9b765
cadema <32099615+cadema@users.noreply.github.com>
2019-03-15 12:15:42 -0400

    Updated xbee3.h code

--- Code/xbee3.h
+++ Code/xbee3.h
@@ -4,8 +4,13 @@
 #ifndef XBEE3_H_
 #define XBEE3_H_
 
+#define ENTER_AT_COMMAND_MODE "+++"
 
+void xbee3_initialize();
 
+void xbee3_broadcast(char *);
+
+void xbee3_uart_receive_string(char *);
 
 
 #endif /* XBEE3_H_ */
